cf7bbd01bff1e1867724a7c8abd8af37da7c687e
Michael Frey <mail@mfrey.net>
2012-05-02 07:55:47 +0200

    Added a header for a unit test

---
+++ sub/de.hu_berlin.informatik.metrik.daemon/include/ConfigurationTest.h
@@ -0,0 +1,49 @@
+#ifndef _STUB_TEST_H_
+#define _STUB_TEST_H_ 
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "Configuration.h"
+
+#include "log4cxx/logger.h"
+
+using namespace std;
+using namespace de::hu_berlin::informatik::metrik::daemon;
+
+/**
+ * THIS IS A STUB: PLEASE FILL ME WITH LIFE.
+ */
+namespace de {
+  namespace hu_berlin {
+    namespace informatik {
+      namespace metrik {
+	namespace daemon {
+          namespace test {
+            class ConfigurationTest : public CppUnit::TestFixture {
+              private:
+                CPPUNIT_TEST_SUITE(ConfigurationTest);
+                CPPUNIT_TEST(testReadConfigurationFile);
+                CPPUNIT_TEST_SUITE_END();
+
+                /// A logging instance
+                static log4cxx::LoggerPtr mLogger;
+                // Instances of the class which will be tested
+                Configuration *c;
+
+              public:
+                /// The set up method of the unit test
+                void setUp(void);
+                /// The tear down method of the unit test
+                void tearDown(void);
+                ///
+                void testReadConfigurationFile();
+            };
+          }
+        }
+      }
+    }
+  }
+}
+
+#endif
