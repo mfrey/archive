23af42889eb62ab67971ffdd17aa6c10c89b37b1
Michael Frey <frey@inf.fu-berlin.de>
2012-10-09 13:52:10 +0200

    added repetition to awk command in shell script

--- pandora-analysis/evaluation_nl.sh
+++ pandora-analysis/evaluation_nl.sh
@@ -3,7 +3,7 @@
 # poor man's debugging (switch)
 TEST=''
 # the variable specifies the correspondend experiment series
-EXPERIMENTS="2 3 4 5 6 7 8 9 10"
+EXPERIMENTS="0"
 # the alpha paramter which is of interest for analysis
 ALPHA_LIST="1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8 3.9 4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8 4.9 5.0"
 # the beta paramter which is of interest for analysis
@@ -20,8 +20,8 @@ for EXPERIMENT in $EXPERIMENTS; do
       EXPERIMENT_DIR='experiment-'$EXPERIMENT'-'$PACKETS'-'$ALPHA'-'$BETA
 	  cd $EXPERIMENT_DIR
 	  for REPETITION in $REPETITIONS; do
-	    # experiment, packet, alpha, beta, route
-	    awk -F "," '{print '$EXPERIMENT'","$1","'$ALPHA'","'$BETA'","$2","$3}' $REPETITION'/'route_pheromone_log.csv >> ../../route_pheromone_final.csv 
+	    # experiment, repetition, packet, alpha, beta, route
+	    awk -F "," '{print '$EXPERIMENT'","'$REPETITION'","$1","'$ALPHA'","'$BETA'","$2","$3}' $REPETITION'/'route_pheromone_log.csv >> ../../route_pheromone_final.csv 
 	  done;
 	  cd ..
     done;
